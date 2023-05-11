84df868a160fb09dfd0ba233aadfa7e0642be398
Violetta <94271862+burlakova-veta@users.noreply.github.com>
2023-05-11 20:40:14 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace csharptest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[2];
+            Console.WriteLine("Введите 2 целых числа");
+            for (int i = 0; i < 2; i++)
+            {
+                num[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = num[0]; i <= num[1]; i++)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
